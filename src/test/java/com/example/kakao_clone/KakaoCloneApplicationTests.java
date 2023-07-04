c76696770e2aef6897a71b2db7fb6bba0dd82be2
SoMi Park <fsm1204@naver.com>
2023-07-04 18:46:44 +0900

    initial commit

---
+++ src/test/java/com/example/kakao_clone/KakaoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakao_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
